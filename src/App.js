7b1f718fbe5fecdc78780145adf9e991d325112e
Tomas <tomas.milkintas1@gmail.com>
2020-09-03 16:34:13 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                         <h1>Login</h1>
                     </Route>
                     <Route path="/">
+                        <Header />
                         <h1>Homepage</h1>
                     </Route>
                 </Switch>
